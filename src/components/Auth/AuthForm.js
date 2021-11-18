94aca88f0996628c4a4f26d83f4fabc376eb395f
utkarshd07 <49608634+utkarshd07@users.noreply.github.com>
2021-11-18 23:53:05 +0530

    React Auth

---
+++ src/components/Auth/AuthForm.js
@@ -0,0 +1,53 @@
+import { useState, useRef } from "react";
+
+import classes from "./AuthForm.module.css";
+
+const AuthForm = () => {
+  const [isLogin, setIsLogin] = useState(true);
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
+
+  const switchAuthModeHandler = () => {
+    setIsLogin((prevState) => !prevState);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const enteredEmail = emailInputRef.current.value;
+    const enteredPassword = passwordInputRef.current.value;
+
+    if (isLogin) {
+    } else {
+      fetch('');
+    }
+  };
+
+  return (
+    <section className={classes.auth}>
+      <h1>{isLogin ? "Login" : "Sign Up"}</h1>
+      <form>
+        <div className={classes.control}>
+          <label htmlFor="email">Your Email</label>
+          <input type="email" id="email" required requiredref={emailInputRef} />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="password">Your Password</label>
+          <input type="password" id="password" requiredref={passwordInputRef} />
+        </div>
+        <div className={classes.actions}>
+          <button>{isLogin ? "Login" : "Create Account"}</button>
+          <button
+            type="button"
+            className={classes.toggle}
+            onClick={switchAuthModeHandler}
+          >
+            {isLogin ? "Create new account" : "Login with existing account"}
+          </button>
+        </div>
+      </form>
+    </section>
+  );
+};
+
+export default AuthForm;
